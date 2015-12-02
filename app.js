f66f9d3c1d6bf4b9d5ed0dbe7b6fd3c867c09618
nathan-siu <nathansiu9730@gmail.com>
2015-12-02 02:52:50 -0500

    routing

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(passport.session());
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/todos', todos);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
